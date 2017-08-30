9cb7a3c8f13cea9f40643e066a737619000e9eb1
Carsten Witt <carsten.witt@gmail.com>
2017-08-30 09:55:19 +0200

    Added test classes

---
+++ tests/src/SalesmanProviderTraitTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace tests;
+
+use Germania\Salesmen\SalesmanProviderTrait;
+
+class SalesmanProviderTraitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetInterceptor()
+    {
+        $mock = $this->getMockForTrait(SalesmanProviderTrait::class);
+
+        $salesman_id = 3;
+
+        // Trait introduces this attribute
+        $this->assertObjectHasAttribute('salesman_id', $mock);
+        $mock->salesman_id = $salesman_id;
+
+        $this->assertEquals( $salesman_id, $mock->getSalesmanId());
+    }
+}
