4f349bd75996a491e46971ecb9e39ad984bc3897
Carsten Witt <carsten.witt@gmail.com>
2017-09-06 09:24:41 +0200

    require_once

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,4 +12,4 @@ $autoloader_file = __DIR__ . '/../vendor/autoload.php';
 if (!is_readable( $autoloader_file )) {
     die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer install' first." . PHP_EOL . PHP_EOL );
 }
-return include $autoloader_file;
+require_once $autoloader_file;
