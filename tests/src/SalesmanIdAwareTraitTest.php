13853874a2b7a24f7948662568cd20be47e60799
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2018-02-08 11:02:00 +0100

    Rename test classes

---
+++ tests/src/SalesmanIdAwareTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace tests;
+
+use Germania\Salesmen\SalesmanIdAwareTrait;
+use Germania\Salesmen\SalesmanIdProviderInterface;
+
+class SalesmanIdAwareTraitTest extends \PHPUnit\Framework\TestCase
+{
+    public function testGetterAndSetter()
+    {
+        $mock = $this->getMockForTrait(SalesmanIdAwareTrait::class);
+
+        $salesman_id = 3;
+
+        // Make sure we are really changing the number here
+        $this->assertNotEquals( $salesman_id, $mock->getSalesmanId());
+
+        $mock->setSalesmanId($salesman_id);
+        $this->assertEquals( $salesman_id, $mock->getSalesmanId());
+    }
+
+
+    public function testSetterWithRetailerNumberProviderInterface()
+    {
+        $mock = $this->getMockForTrait(SalesmanIdAwareTrait::class);
+
+        // Make sure we are really changing the number here
+        $salesman_id = 3;
+        $this->assertNotEquals( $salesman_id, $mock->getSalesmanId());
+
+        $provider = $this->prophesize( SalesmanIdProviderInterface::class );
+        $provider->getSalesmanId()->willReturn( $salesman_id );
+        $mock->setSalesmanId( $provider->reveal() );
+
+        $this->assertEquals( $salesman_id, $mock->getSalesmanId());
+    }
+}
---
+++ tests/src/SalesmanIdProviderTraitTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace tests;
+
+use Germania\Salesmen\SalesmanIdProviderTrait;
+
+class SalesmanIdProviderTraitTest extends \PHPUnit\Framework\TestCase
+{
+    public function testGetInterceptor()
+    {
+        $mock = $this->getMockForTrait(SalesmanIdProviderTrait::class);
+
+        $salesman_id = 3;
+
+        // Trait introduces this attribute
+        $this->assertObjectHasAttribute('salesman_id', $mock);
+        $mock->salesman_id = $salesman_id;
+
+        $this->assertEquals( $salesman_id, $mock->getSalesmanId());
+    }
+}
